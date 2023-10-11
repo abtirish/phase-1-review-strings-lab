a20a4c27ca476568b11aa52a395d446214f426fc
abtirish <abtirish@gmail.com>
2023-10-10 18:41:47 -0600

    finish phase 1 review strings lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code in this file!
+const currentUser = 'Amanda'
+
+
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!'
+
+console.log(welcomeMessage);
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
+
+console.log(shortGreeting)
+
+
